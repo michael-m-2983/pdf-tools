c30240feae19d18dea77b4055625ffe464a647e2
michael-m-2983 <michaelmccright2983@gmail.com>
2024-09-21 19:36:43 -0500

    Add PDF rotating features

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
-document.getElementById("finish-button")!.onclick = function(_: MouseEvent) {
-  // Get Input PDF
-  const fileSelectElement: HTMLInputElement = document.getElementById("file-select")! as HTMLInputElement;
+import { PDFDocument, degrees } from 'pdf-lib'
 
-  //TODO: Validate that there is a file
-  //TODO: Validate the type of file
-  for(let i = 0; i < fileSelectElement.files!.length; i++) {
-    alert(fileSelectElement.files!.item(i)?.name);
+document.getElementById("finish-button")!.onclick = async function(_: MouseEvent) {
+  const fileSelectElement: HTMLInputElement = document.getElementById("file-select")! as HTMLInputElement;
+  const action: string = (document.getElementById("action") as HTMLInputElement).value;
+
+  if(fileSelectElement.files!.length != 1) {
+    alert("You must upload a single PDF file!");
+    return;
+  }
+
+  const file: File = fileSelectElement.files!.item(0)!;
+
+  const doc = await PDFDocument.load(await file.arrayBuffer());
+
+  const rotate = (deg: number) => doc.getPages().forEach(page => page.setRotation(degrees(deg)));
+
+  switch(action) {
+    case "rotate90":
+      rotate(90);
+      break;
+    case "rotate180":
+      rotate(180);
+      break;
+    case "rotate270":
+      rotate(270);
+      break;
   }
   
-  // Perform action
-  // Download PDF
+  window.open(await doc.saveAsBase64({dataUri: true}));
 }
