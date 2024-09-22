3f769d786ac8b1fdc2b63e6058431ebc97640c6d
michael-m-2983 <michaelmccright2983@gmail.com>
2024-09-22 12:54:58 -0500

    Add metadata option

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { PDFDocument, degrees } from 'pdf-lib'
+import { PDFDocument, PDFPage, degrees } from 'pdf-lib'
 
-document.getElementById("finish-button")!.onclick = async function(_: MouseEvent) {
+document.getElementById("finish-button")!.onclick = async function (_: MouseEvent) {
   const fileSelectElement: HTMLInputElement = document.getElementById("file-select")! as HTMLInputElement;
   const action: string = (document.getElementById("action") as HTMLInputElement).value;
 
-  if(fileSelectElement.files!.length != 1) {
+  if (fileSelectElement.files!.length != 1) {
     alert("You must upload a single PDF file!");
     return;
   }
@@ -15,7 +15,7 @@ document.getElementById("finish-button")!.onclick = async function(_: MouseEvent
 
   const rotate = (deg: number) => doc.getPages().forEach(page => page.setRotation(degrees(deg)));
 
-  switch(action) {
+  switch (action) {
     case "rotate90":
       rotate(90);
       break;
@@ -30,7 +30,29 @@ document.getElementById("finish-button")!.onclick = async function(_: MouseEvent
         page.drawText((index + 1).toString(), { x: 10, y: 10 });
       });
       break;
+    case "metadata":
+      let page = doc.insertPage(0);
+      const metadata = {
+        "Author": doc.getAuthor(),
+        "Creation Date": doc.getCreationDate(),
+        "Creator": doc.getCreator(),
+        "Keywords": doc.getKeywords(),
+        "Modification Date": doc.getModificationDate(),
+        "Producer": doc.getProducer(),
+        "Subject": doc.getSubject(),
+        "Title": doc.getTitle()
+      }
+      Object.entries(metadata).forEach(([key, value], index) => {
+        page.drawText(key + ": " + value?.toString(), {
+          x: 30,
+          y: page.getHeight() - 60 - (index * 15),
+          size: 10
+        })
+      });
+      break;
   }
 
-  window.open(await doc.saveAsBase64({dataUri: true}), "_blank")?.focus();
+  doc.setProducer("pdf-tools (https://github.com/michael-m-2983/pdf-tools)");
+
+  window.open(await doc.saveAsBase64({ dataUri: true }), "_blank")?.focus();
 }
