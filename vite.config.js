4c9c6e95832878798f2e4f3d0a3c787b391d4492
michael-m-2983 <michaelmccright2983@gmail.com>
2024-10-06 16:30:41 -0500

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from "node:path";
 import process from "node:process";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   root: "src",
@@ -10,5 +11,27 @@ export default defineConfig({
   resolve: {
     alias: { "/src": path.resolve(process.cwd(), "src") }
   },
-  base: "/pdf-tools/"
+  base: "/pdf-tools/",
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo.svg",
+            purpose: "any",
+            sizes: "any",
+            type: "image/svg+xml"
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      },
+      srcDir: "src",
+      base: "/pdf-tools/",
+      injectRegister: 'auto',
+    })
+  ]
 });
