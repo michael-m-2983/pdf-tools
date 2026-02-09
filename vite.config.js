39f3e6e618e3b26f6d8c96439c17a545fe6b1542
michael-m-2983 <michaelmccright2983@gmail.com>
2026-02-09 15:49:02 -0600

    Add build configuration modes

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,64 @@ import path from "node:path";
 import process from "node:process";
 import { defineConfig } from "vite";
 import { VitePWA } from 'vite-plugin-pwa'
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+const buildMode = process.env.BUILD_MODE || "offline-html";
+
+let plugins = [];
+let basePath = "";
+switch (buildMode) {
+	case "normal":
+		plugins = [
+			VitePWA({
+				registerType: 'autoUpdate',
+				manifest: {
+					theme_color: "#ffffff",
+					icons: [
+						{
+							src: "/logo.svg",
+							purpose: "any",
+							sizes: "any",
+							type: "image/svg+xml"
+						}
+					]
+				},
+				injectRegister: 'auto'
+			})
+		];
+		break;
+	case "github-pages":
+		basePath = "/pdf-tools";
+		plugins = [
+			VitePWA({
+				registerType: 'autoUpdate',
+				manifest: {
+					theme_color: "#ffffff",
+					icons: [
+						{
+							src: "/pdf-tools/logo.svg",
+							purpose: "any",
+							sizes: "any",
+							type: "image/svg+xml"
+						}
+					]
+				},
+				devOptions: {
+					enabled: true
+				},
+				base: "/pdf-tools/",
+				injectRegister: 'auto'
+			})
+		];
+		break;
+	case "offline-html":
+		plugins = [
+			viteSingleFile()
+		];
+		break;
+}
 
 export default defineConfig({
-  base: "/pdf-tools/",
-  plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/pdf-tools/logo.svg",
-            purpose: "any",
-            sizes: "any",
-            type: "image/svg+xml"
-          }
-        ]
-      },
-      devOptions: {
-        enabled: true
-      },
-      base: "/pdf-tools/",
-      injectRegister: 'auto'
-    })
-  ]
+	base: basePath,
+	plugins: plugins
 });
